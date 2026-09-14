fc766a51d57509a47a73443b42c4b966e3b75bc8
Turjoy-barua <baruaturjoy826@gmail.com>
2026-09-14 13:35:41 +0200

    files added for tme0

--- tp1.py
+++ tp1.py
@@ -96,8 +96,6 @@ print(test_nombre(120))
 
 # exercice6
 def airetiangle(a: int, b: int, c: int) -> float:
-<<<<<<< HEAD
-=======
     """
     Args:
         a (int): côté a
@@ -107,11 +105,9 @@ def airetiangle(a: int, b: int, c: int) -> float:
     Returns:
         float: aire du triangle 
     """
->>>>>>> ccbb82c1ea829a18d701295bf2d6e451811aadf5
     p: float = (a+b+c)/2
     return math.sqrt(p*(p-a)*(p-b)*(p-c))
     
-
 def volumetetraedre(a: float, b: float, c: float, d: float, e: float, f: float) -> float:
     """
 
