c23ac7023786085934d30fa8886541199b6cac2b
Turjoy-barua <baruaturjoy826@gmail.com>
2026-09-15 17:44:52 +0200

    modified k file

--- TME/K.py
+++ TME/K.py
@@ -34,21 +34,21 @@ def somme_entier_pairs(n):
 #-----Question_1-----------
 
 def trouver_nombre():
-   # n = randint(0, 100)
-    n = 10
+    n = randint(0, 100)
     nb_coup = 0
 
     while True:
         proposition_joueur = int(input("Veuillez choisir un nombre entre 0 et 100"))
         if proposition_joueur > n:
             nb_coup += 1
-            print("plus grand")
+            print("plus petit")
             continue
 
         elif proposition_joueur < n:
             nb_coup += 1
-            print("plus petit")
+            print("plus grand")
             continue
         elif proposition_joueur == n:
-            print("Vous avez gagné, vous avez joué {nb_coup} coups")
+            print(f"Vous avez gagné, vous avez joué {nb_coup} coups")
+            break
 trouver_nombre()
