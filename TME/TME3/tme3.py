5d0f6aaff3d61e806f1ae30eb68503b111e498c1
Turjoy-barua <baruaturjoy826@gmail.com>
2026-09-17 11:11:58 +0200

    tme 3 file added

---
+++ TME/TME3/tme3.py
@@ -0,0 +1,13 @@
+"""
+Exercice: TP3.ex1
+Nom: BARUA et KEGREISZ
+Date creation: 17/09/2026
+"""
+
+
+def decode_rle(code: str):
+    for x in str:
+        print(str)
+        
+
+decode_rle(“4a3b1c3d")
