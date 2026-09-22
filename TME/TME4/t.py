23082bd84be16c3be669b6e27020307b2f47c6e1
Turjoy-barua <baruaturjoy826@gmail.com>
2026-09-22 17:13:57 +0200

    new test file added for tme4

--- TME/TME4/t.py
+++ TME/TME4/t.py
@@ -19,24 +19,11 @@ def decomposition(n : int) -> list:
   
 #print(decomposition(3))
 
+def encadrements(n : int) -> list:
+    numbers = []
+    for i in range(n):
+        numbers.append(i+1)
+    final_list = [(i, j, k) for i in numbers for j in numbers for k in numbers if i <= j <= k]
+    return final_list
 
-def d_to_b(n, b):
-    
-    char = '0b'
-    binaire = ''
-    binaire_list = []
-    while n > 0:
-        binaire = str(n % 2) + binaire
-        n //= 2
-    
-    #while 0 < n < 2**b:
-     #   if n // 2 == 0:
-      #      binaire+= '0'
-       # else: 
-        #    binaire+= '1'
-        #n = n/2
-    char = char + binaire
-    print(binaire)
-    print(char)
-    
-d_to_b(8, 8)
+print(encadrements(2))
