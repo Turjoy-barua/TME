4c25e50d2004b422b2e04bd8136754ba27371d47
Turjoy-barua <baruaturjoy826@gmail.com>
2026-09-23 16:37:50 +0200

    updates of tme4 but not finished

--- TME/TME4/t.py
+++ TME/TME4/t.py
@@ -27,3 +27,21 @@ def encadrements(n : int) -> list:
     return final_list
 
 print(encadrements(2))
+
+def compte(nom):
+    f = open(nom)
+    text = str(f.read())
+    text_words = text.split()
+    counters = {}
+    for x in set(text_words):
+        counts = 0
+        for y in text_words:
+            if x == y:
+                counts+=1
+        counters.update({x: counts})
+    for key, value in counters.items():
+        print(key , ':', value)
+    return counters
+
+
+compte('/Users/TURJOY/Documents/shared_university /TME/TME4/miserables-chap1-tokenized.txt')
